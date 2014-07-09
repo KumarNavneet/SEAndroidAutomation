5ee46434607d0c1c49b66809dea3c16d3c7984f8
Navneet <navneet@localhost.localdomain>
2014-07-09 19:32:40 +0530

    begining of parsing of rules

--- SEAndroidAutomation.java
+++ SEAndroidAutomation.java
@@ -3,9 +3,9 @@ public class SEAndroidAutomation{
 	public static void main(String args[]){
 		String dirName = args[0];
 		String mkdirCmd[] = {"mkdir", dirName};
-		String cdDirCmd[] = {"cd", dirName};
+		//String cdDirCmd[] = {"cd", dirName};
 		String sepolicyPullCmd[] = {"adb", "pull","sepolicy","./"+dirName};
-		String bugReportCmd[] = {"adb", "bugReport"};
+		String bugReportCmd[] = {"adb", "bugreport"};
 		String ruleCmd[] = {"audit2allow", "./"+dirName+"/audit.log"};//,">","Rules"};
 		String ruleCmd2[] = {"cat", "Rules", "|", "grep ","allow"};//,">","OnlyRules"};
 		try{	
@@ -24,50 +24,66 @@ public class SEAndroidAutomation{
 			pb.redirectErrorStream(true);
 			pr = pb.start();
 			pr.waitFor();
-
+			
+			String temp = "";
+			BufferedReader br = null;
+			File f = null;
+			FileWriter fw = null;
+			BufferedWriter bw = null;
 			//pb = new ProcessBuilder(bugReportCmd);
 			//pb.redirectErrorStream(true);
 			//pr = pb.start();
 			//pr.waitFor();
-
+			
 			pr = Runtime.getRuntime().exec("adb bugreport");
-			BufferedReader br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-			File f = new File("./"+dirName+"/audit.log");
-			FileWriter fw = new FileWriter(f.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
+			br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			f = new File("./"+dirName+"/audit.log");
+			fw = new FileWriter(f.getAbsoluteFile());
+			bw = new BufferedWriter(fw);
 			//DataOutputStream os = new DataOutputStream(pr.getOutputStream());            
             		//for (String tmpCmd : bugReportCmd) 
             		//      os.writeBytes(tmpCmd+"\n");
-			String temp = br.readLine()+"\n";
+			String temp = br.readLine();
+			//int c = 0;
 			while(temp != null){
+					temp += "\n";
 					bw.write(temp);
-					temp = br.readLine()+"\n";
+					temp = br.readLine();
+					//c++;
 			}
 			bw.close();
 			System.out.println("Done Writing the Bugreport to ./"+dirName+"audit.log");
-			
+
 			pr = Runtime.getRuntime().exec("audit2allow ./"+dirName+"/audit.log");
 			br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            		f = new File("./"+dirName+"/Rules.txt");
+            		f = new File("./"+dirName+"/Rules");
 			fw = new FileWriter(f.getAbsoluteFile());
 			bw = new BufferedWriter(fw);
-			temp = br.readLine()+"\n";
+			temp = br.readLine();
 			while(temp != null){
+				temp += "\n";
 				bw.write(temp);
-				temp = br.readLine()+"\n";			
+				temp = br.readLine();			
 			}
-			pr = Runtime.getRuntime().exec("cat ./"+dirName+"/Rules.txt"+" | grep allow");
-			br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            		f = new File("./"+dirName+"/OnlyRules.txt");
-			fw = new FileWriter(f.getAbsoluteFile());
-			bw = new BufferedWriter(fw);
-			temp = br.readLine()+"\n";
-			while(temp != null){
-				bw.write(temp);
-				temp = br.readLine()+"\n";			
-			}
-			System.out.println("Done Writing the Rules to ./"+dirName+"OnlyRules.txt");
 			
+			pr = Runtime.getRuntime().exec("cat ./"+dirName+"/Rules"+" | grep allow");
+			br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            		f = new File("./"+dirName+"/OnlyRules");
+			fw = new FileWriter(f.getAbsoluteFile());
+			bw = new BufferedWriter(fw);
+			temp = br.readLine();
+			while(temp != null){
+				temp += "\n";
+				bw.write(temp);
+				temp = br.readLine();		
+			}
+			System.out.println("Done Writing the Rules to ./"+dirName+"OnlyRules");
+			
+			br = new BufferedReader(new FileReader("./"+dirName+"/OnlyRules"));
+			while((temp = br.readLine()) != null){
+				pr = Runtime.getRuntime().exec("sepolicy-inject ");
+				System.out.println(temp);
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
