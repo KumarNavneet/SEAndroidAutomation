a0e68f12c5ba835eb9a515e27dbb45e43fd98acd
Navneet <navneet@localhost.localdomain>
2014-07-10 17:41:24 +0530

    Done with user rule selection...need to do parsing now

--- SEAndroidAutomation.java
+++ SEAndroidAutomation.java
@@ -1,35 +1,58 @@
 import java.io.*;
 import javax.swing.*;
-public class SEAndroidAutomation{
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class SEAndroidAutomation extends JFrame{
+	private static JPanel topPanel;
+	private static JButton ok;
+	private static JList lst;
+	public static List<String> rulesByUser = new ArrayList<>();
 	public static void main(String args[]){
 		String dirName = args[0];
+		int i=0;
 		String mkdirCmd[] = {"mkdir", dirName};
 		//String cdDirCmd[] = {"cd", dirName};
-		String sepolicyPullCmd[] = {"adb", "pull","sepolicy","./"+dirName};
+		String sepolicyPullCmd[] = {"adb", "pull","sepolicy","./"+dirName+"/origSePolicy"};
 		String bugReportCmd[] = {"adb", "bugreport"};
-		String ruleCmd[] = {"audit2allow", "-i"+"audit.log","-o"+"./"+dirName+"/Rules"};// -p ./"+dirName+"/sepolicy"};//,">","Rules"};
+		String ruleCmd[] = {"audit2allow", "-i"+"audit.log","-o"+"./"+dirName+"/Rules"};// -p ./"+dirName+"/origSepolicy"};
+		String permDomainCmd[] = {"seinfo", "--permissive", "mySepolicy"};//"./"+dirName+"/origSepolicy"};
+		List<String> rules = new ArrayList<>();
+		List<String> permissiveDomains = new ArrayList<>();
+		String temp = "";
+		BufferedReader br = null;
+		File f = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
 		try{	
 			Runtime rt = Runtime.getRuntime();
 		
 			ProcessBuilder pb = new ProcessBuilder(mkdirCmd);
 			pb.redirectErrorStream(true);
 			Process pr = pb.start();
+			pr.waitFor();
+
+			pb = new ProcessBuilder(sepolicyPullCmd);
+			pb.redirectErrorStream(true);
+			pr = pb.start();
 			pr.waitFor();		
 		
 			//pb = new ProcessBuilder(cdDirCmd);
 			//pr = pb.start();
 			//pr.waitFor();
-		
-			pb = new ProcessBuilder(sepolicyPullCmd);
+			pb = new ProcessBuilder(permDomainCmd);
 			pb.redirectErrorStream(true);
 			pr = pb.start();
 			pr.waitFor();
+			br = new BufferedReader(new InputStreamReader(pr.getInputStream()));			
+			temp = br.readLine();
+			while(temp != null){
+					System.out.println("Permissive Domain : "+temp);
+					permissiveDomains.add(temp);
+					temp = br.readLine();
+			}
+			br.close();			
 			
-			String temp = "";
-			BufferedReader br = null;
-			File f = null;
-			FileWriter fw = null;
-			BufferedWriter bw = null;
 			//pb = new ProcessBuilder(bugReportCmd);
 			//pb.redirectErrorStream(true);
 			//pr = pb.start();
@@ -62,11 +85,9 @@ public class SEAndroidAutomation{
 			fw = new FileWriter(f.getAbsoluteFile());
 			bw = new BufferedWriter(fw);
 			temp = br.readLine();
-			String rules[] = new String[20];
-			int i=0;
 			while(temp != null){
 				temp += "\n";
-				rules[i++]=temp;
+				rules.add(temp);
 				bw.write(temp);
 				temp = br.readLine();			
 			}
@@ -74,17 +95,42 @@ public class SEAndroidAutomation{
 			bw.close();
 			
 			System.out.println("Done Writing the Rules to ./"+dirName+"/OnlyRules");
-	
+			/*
+			JFrame RuleFrame = new JFrame();
+			RuleFrame.setTitle("Select Rules to add");
+			RuleFrame.setSize(400,400);
+			RuleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			topPanel = new JPanel();
+			topPanel.setLayout(new BorderLayout());
+			RuleFrame.getContentPane().add(topPanel);
+
+			lst = new JList(rules);
+			topPanel.add(lst, BorderLayout.CENTER);
+			
+			ok = new JButton("Ok");
+			ok.setPreferredSize(new Dimension(50, 50));
+			topPanel.add(ok, BorderLayout.SOUTH);
+			ok.addActionListener(this);
+			RuleFrame.setVisible(true);
+			
+			*/
 			RulesFrame obj = new RulesFrame(rules);
 			obj.setVisible(true);
+			/*
 			br = new BufferedReader(new FileReader("./"+dirName+"/OnlyRules"));
 			while((temp = br.readLine()) != null){
 				//pr = Runtime.getRuntime().exec("sepolicy-inject ");
 				System.out.println(temp);
-			}
+			}*/
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		
 	}
+	public static void afterOkClick(){
+		System.out.println("you clicked on OK");
+		
+	}
+	
 }
